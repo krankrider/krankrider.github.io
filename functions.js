4429405f1797d2fc047377888e682323c890bfbf
krankrider <168082363+krankrider@users.noreply.github.com>
2024-12-09 09:45:37 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -2,16 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const themeToggleButton = document.getElementById("theme-toggle");
   const htmlElement = document.documentElement;
 
+  const updateTheme = (theme) => {
+    htmlElement.setAttribute("data-theme", theme);
+    htmlElement.classList.toggle("dark-theme", theme === "dark");
+    localStorage.setItem("theme", theme);
+  };
+
   let currentTheme = localStorage.getItem("theme") || "light";
-  htmlElement.setAttribute("data-theme", currentTheme);
-  htmlElement.classList.toggle("dark-theme", currentTheme === "dark");
+  updateTheme(currentTheme);
 
   themeToggleButton.addEventListener("click", () => {
     currentTheme = currentTheme === "light" ? "dark" : "light";
-
-    htmlElement.setAttribute("data-theme", currentTheme);
-    htmlElement.classList.toggle("dark-theme", currentTheme === "dark");
-
-    localStorage.setItem("theme", currentTheme);
+    updateTheme(currentTheme);
   });
+
+  const updateClock = () => {
+    const now = new Date();
+    const formattedTime = now.toLocaleTimeString("en-GB", { hour12: false });
+    document.getElementById("clock").textContent = formattedTime;
+  };
+
+  setInterval(updateClock, 1000);
+  updateClock();
 });
